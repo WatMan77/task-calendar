2bf9d961f0451ebc02778f460aa901db38df2c48
Sami Kuikka <sami.kuikka@aalto.fi>
2022-05-14 11:26:10 +0300

    dummy module v.2

--- src/components/DummyForm.js
+++ src/components/DummyForm.js
@@ -18,11 +18,32 @@ function DummyForm() {
 
   const { dummy_open, openDummy, dummy_task } = useContext(CalendarContext);
 
-  
+    const [name, setName] = useState("");
+    const [color, setColor] = useState("#4caf50");
+    const [error, setError] = useState(false);
+    const [errorText, setErrorText] = useState("");
+
+    useEffect( () => {
+        if(dummy_task) {
+            setName(dummy_task.name || "");
+            setColor(dummy_task.color || "#4caf50")
+        }
+    }, [dummy_task]);
 
   const closeModal = () => {
     openDummy(false);
+    setError(false);
   };
+
+  const _noDelete = () => {
+      setError(true);
+      setErrorText("You can not delete important tasks!");
+  }
+
+  const _noModify = () => {
+      setError(true);
+      setErrorText("You can not change important task!");
+  }
 
   return (
     <Modal
@@ -32,9 +53,44 @@ function DummyForm() {
       ariaHideApp={false}
       contentLabel="Task Form"
     >
-      <button className="button button-red" onClick={closeModal}>
-            Cancel
-        </button>
+        <div className="task-form">
+            <label>Name</label>
+            <input
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Task name"
+            />
+            <label>Color</label>
+
+            <div>
+            <CirclePicker
+                color={color}
+                onChange={(color) => {
+                setColor(color.hex);
+                }}
+            />
+            </div>
+                <div>
+                    <button className="button button-red" onClick={closeModal}>
+                        Cancel
+                    </button>
+                    <button
+                    className="button button-orange"
+                    onClick={_noDelete}
+                    >
+                    Delete
+                    </button>
+                    <button
+                    className="button button-green"
+                    onClick={_noModify}
+                >
+                    Save
+                </button>
+            </div>
+            {error ? <p className="error">{errorText}</p> : null}
+        </div>
     </Modal>
   );
 }
