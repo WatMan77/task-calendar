d76461f904cd0b294f1b2659c40a34e1fdd00650
daniel <dansku110@gmail.com>
2022-05-20 13:41:03 +0300

    small class fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   
   return (
     <div className="container">
-      <div class="welcomeMsg">
+      <div className="welcomeMsg">
         <h1 >Welcome to your daily helper!</h1>
       </div>      
       <CalendarState>
